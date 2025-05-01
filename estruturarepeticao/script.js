c7db7f9e32fe1960e190f0393a89ee8b3bb60322
rosa7788 <rosennigt7@gmail.com>
2025-04-30 22:09:53 -0300

    oxe

---
+++ estruturarepeticao/script.js
@@ -0,0 +1,16 @@
+/* function oxe0(){
+    let nota //guarda as notas
+    let conta = 1; //conta de 1 a 6
+    let soma = 0
+    while (conta<=6){
+        nota = Number(prompt(`Informe a nota ${conta}`));
+        soma = soma + nota;
+        conta++; //conta = conta + 1
+    }
+    let media = soma /6
+    //mostra o resultado
+    alert (`A media das notas é: ${media.toFixed(1)}`)
+}
+*/ 
+
+
