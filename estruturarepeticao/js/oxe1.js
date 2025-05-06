d9a7631ef72b5f122b581e3c684e91f34d915c3e
rosa7788 <rosennigt7@gmail.com>
2025-05-06 18:40:06 -0300

    pesquisa

--- estruturarepeticao/js/oxe1.js
+++ estruturarepeticao/js/oxe1.js
@@ -3,24 +3,28 @@ function pesquisaSatisfacao (){
     let nota; 
     let participantes = 10;
     let satisfeitos = 0;
+    let meioTermo = 0;
     let insatisfeitos = 0;
     let conta = 1;
 
     while (conta<= participantes){
-        nota = Number(prompt(`Qual a nota de participação ${conta}:`));
+        nota = Number(prompt(`Qual a nota de participação: ${conta}`));
         conta++;
         soma += nota;
-        if (nota<=8){
+        if (nota >= 8){
             satisfeitos++;
-        } else if(nota<5){
+        } else if (nota < 8 && nota > 5){
+            meioTermo++;
+        } else{
             insatisfeitos++;
-        } 
+        }  
     }
 
     let media = soma/participantes
     alert(`A média das notas é: ${media}`);
     alert(`Participantes satisfeitos: ${satisfeitos}`);
     alert(`Participantes insatisfeitos: ${insatisfeitos}`);
+    alert(`Participantes de meio termo: ${meioTermo}`);
 
 
 }
