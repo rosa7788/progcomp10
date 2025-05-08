010d7cf9e6f17eeb0927e5211d404632fd7a6927
rosa7788 <rosennigt7@gmail.com>
2025-05-07 22:06:50 -0300

    oxe

--- estruturarepeticao/js/oxe1.js
+++ estruturarepeticao/js/oxe1.js
@@ -1,26 +1,36 @@
 function pesquisaSatisfacao (){
     let soma = 0;
-    let nota; 
-    let participantes = 10;
-    let satisfeitos = 0;
-    let insatisfeitos = 0;
-    let conta = 1;
+    let nota; //sera informado pelo usuario
+    let participantes = 10; 
+    let satisfeitos = 0; //incial ninguem
+    let insatisfeitos = 0; //inicial ninguem
+    let conta = 1; //varia de 1 a 10
 
     while (conta<= participantes){
         nota = Number(prompt(`Qual a nota de participação: ${conta}`));
-        conta++;
-        soma += nota;
-        if (nota >= 8){
-            satisfeitos++;
-        } else if (nota<5){
-            insatisfeitos++;
+        if (nota >= 8 && nota <= 10){
+            satisfeitos++; //satisfeitos + 1
         } 
+        else if (nota >= 0 && nota < 5){
+            insatisfeitos++;
+        }
+        else if (nota < 0 || nota > 10) {
+            alert(`nota inválida`);
+            continue; //volta para o inicio, sem incrementar conta
+        }
+            soma += nota; //soma = soma + nota
+            conta++; //conta = conta + 1
+         
     }
+    
 
     let media = soma/participantes
     alert(`A média das notas é: ${media}`);
     alert(`Participantes satisfeitos: ${satisfeitos}`);
     alert(`Participantes insatisfeitos: ${insatisfeitos}`);
+
 }
+
+
 
 
