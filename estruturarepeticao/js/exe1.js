366e6dd8333a16fcdff2ecc358d5f4e8ab0c83a7
rosa7788 <rosennigt7@gmail.com>
2025-05-12 20:39:23 -0300

    exe1

--- estruturarepeticao/js/exe1.js
+++ estruturarepeticao/js/exe1.js
@@ -1,10 +1,13 @@
 function exe1 () {
     let a, b, c, d;
+    let grupo = 1
+    while (grupo <= 5){
     let conta = 1
     a = Number(prompt(`Informe o valor do A`))
     b = Number(prompt(`Informe o valor do B`))
     c = Number(prompt(`Informe o valor do C`))
     d = Number(prompt(`Informe o valor do D`))
+    dig = (`${a}, ${b}, ${c}, ${d}`);
 
     while (conta <= 3){
         if (a > b){
@@ -18,8 +21,11 @@ function exe1 () {
         }
         conta++;
     }
+    alert(`Ordem digitada ${dig}`)
     alert(`Ordem crescente ${a} ${b} ${c} ${d}`)
     alert(`Ordem decrescente ${d} ${c} ${b} ${a}`)
+    grupo ++;
+}
     
 
 }
