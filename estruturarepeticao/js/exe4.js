05b531d791bb24556b60b5c7557b05d998adfe9f
rosa7788 <rosennigt7@gmail.com>
2025-05-26 20:18:48 -0300

    exe5

---
+++ estruturarepeticao/js/exe4.js
@@ -0,0 +1,8 @@
+function exe4 (){
+    let numero = Number(prompt(`Informe numero para tabuada`))
+    //estrutura de repeticao
+    for(let conta = 1; conta <= 10; conta++){
+        console.log (`${numero} x ${conta} == ${numero*conta}`)
+    }
+      
+}
---
+++ estruturarepeticao/js/exe5.js
@@ -0,0 +1,7 @@
+function exe5 (){
+
+    for(let conta = 1; conta <= 10; conta++){
+        for (let numero = 1; numero <= 10; numero++)
+        console.log (`${numero} x ${conta} == ${numero*conta}`)
+    } 
+}
