c6991818f0ca0e76fb3df5203215dfc01dea21fd
rosa7788 <rosennigt7@gmail.com>
2025-08-06 20:30:58 -0300

    objeto

---
+++ objetos/obj.js
@@ -0,0 +1,32 @@
+/*let filme = [{
+    nome: "Bailarina",
+    ano: 2020,
+    pais: "Coreia",
+    ator: "Jeong"
+
+}, 
+{
+   nome: "Myname",
+    ano: 2019,
+    pais: "Coreia",
+    ator: "Hansohee"
+
+}
+];
+
+for(let i =0; i<filme.length; i++){
+console.log(`O ator é:${filme[i].ator}`)
+}
+*/
+
+let biblioteca = [];
+
+for (let i = 0; i < 3; i++){
+
+    let titulo = prompt(`Informe o titulo ${i + 1}:`);
+
+}
+
+
+
+
