b9a1fa8eed0fbfe6cee85d47cfa216bc5dd3b230
rosa7788 <rosennigt7@gmail.com>
2025-09-09 22:13:25 -0300

    exe

--- string/beecrowd.js
+++ string/beecrowd.js
@@ -26,3 +26,17 @@ for(let i = 1; i <= quantidadeNumeros;i++){
     console.log(`${soma} leds`)
 
 }
+
+//2779
+
+let total = parseInt(lines[0])
+let pacote = parseInt(lines[1])
+
+let nova = new Set ()
+for (let i = 2; i <= pacote+1;i++){
+    nova.add(lines[i])
+}
+let faltam = total - nova.size
+console.log(faltam)
+
+
