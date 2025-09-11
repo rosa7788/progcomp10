32c14135050d999f694b11fb303a3100d5f870a1
rosa7788 <rosennigt7@gmail.com>
2025-09-10 23:07:55 -0300

    exe

--- string/beecrowd.js
+++ string/beecrowd.js
@@ -64,28 +64,39 @@ while (i < lines.length){
 
 //1179
 
-let par = []
-let impar = []
 
-for(let i = 0; i < lines.length; i++){
-    if (lines[i] % 2 === 0){
-        par.push(lines[i]);
-        if (par.length === 5){ //encheu
-            for(let i = 0; i < 5; i++){
-                console.log(`par[${i}] = ${par[i]}`);
-            }
-            par = []; //esvazia
-        }
-    } else {
-        impar.push(lines[i]);
-        for (let i = 0; i < 5; i++){
-        if(impar.length === 5){
-            console.log(`impar[${i}] = ${impar[i]}`)
-        }
+let lines = input.trim().split("\n")
+
+let par = [];
+let impar = [];
+
+for (let i = 0; i < lines.length; i++) {
+  if (parseInt(lines[i]) % 2 === 0) {
+    par.push(parseInt(lines[i]));
+    if (par.length === 5) { // encheu
+      for (let i = 0; i < 5; i++) {
+        console.log(par{[i]} = ${par[i]});
+      }
+      par = []; // esvazia
     }
-    
-        impar = []
+  } else {
+    impar.push(parseInt(lines[i]));
+    if (impar.length === 5) { // encheu
+      for (let i = 0; i < 5; i++) {
+        console.log(impar[${i}] = ${impar[i]});
+      }
+      impar = []; // esvazia
     }
+  }
+}
+
+// Depois da leitura, imprime o que restou
+for (let i = 0; i < impar.length; i++) {
+  console.log(impar[${i}] = ${impar[i]});
+}
+
+for (let i = 0; i < par.length; i++) {
+  console.log(par[${i}] = ${par[i]});
 }
 
 
